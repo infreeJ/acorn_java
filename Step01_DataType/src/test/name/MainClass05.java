72f9f61731c217acec9cf8601f58b1c4803e2df5
infreeJ <kasious84@gmail.com>
2025-06-16 17:06:05 +0900

    Step01에 MainClass05 추가함

--- Step01_DataType/src/test/name/MainClass05.java
+++ Step01_DataType/src/test/name/MainClass05.java
@@ -1,5 +1,16 @@
 package test.name;
 
 public class MainClass05 {
-
+	public static void main(String[] args) {
+		String name = "김구라";
+		String tmp = name;
+		name = "원숭이";
+		name = null;
+		System.out.println(tmp);
+		System.out.println(name);
+		
+		
+		int num = 10;
+		String yourNameString = "해골";
+	}
 }
