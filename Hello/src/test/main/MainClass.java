d8d671ec20c5c5f48d46137154ca02571a9e2d51
infreeJ <kasious84@gmail.com>
2025-06-16 16:10:37 +0900

    hello Project 추가

---
+++ Hello/src/test/main/MainClass.java
@@ -0,0 +1,28 @@
+package test.main;
+
+//Main 이라는 이름의 클래스 만들기
+public class MainClass {
+	
+	// main 이라는 이름의 메서드를 클래스 안에 만들기
+	public static void main(String[] args) {
+		// main 메서드 안에 있는 코드를 실행하려면 run 버튼 클릭
+		System.out.println("Hello, World!");
+		System.out.println("이준혁");
+		
+		int num = 10;
+		num = 20;
+		num = 30;
+		
+		double num2 = 10.1;
+		num2 = 10.2;
+		num2 = 10.5;
+		
+		boolean isRun = true;
+		isRun = false;
+		isRun = true;
+		
+		String myName = "김구라";
+		myName = "해골";
+		
+	}
+}
